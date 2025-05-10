202aa67d69c9cc9f9406469d455094cd333170d4
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-05-10 20:24:56 +1000

    Add Editor Camera pivot dragging.

--- Assets/LevelEditor/Scripts/EditorController.cs
+++ Assets/LevelEditor/Scripts/EditorController.cs
@@ -5,8 +5,11 @@ public class EditorController : MonoBehaviour
     [Tooltip("Default Camera distance from the pivot point.")]
     public float DefaultDistance = 25.0f;
     
-    [Tooltip("Mouse Dragging Sensitivity.")]
-    public float Sensitivity = 3.0f;
+    [Tooltip("Mouse Orbit Dragging Sensitivity.")]
+    public float OrbitSensitivity = 3.0f;
+
+    [Tooltip("Mouse Pivot Point Dragging Sensitivity.")]
+    public float PivotSensitivity = 1.0f;
 
     [Tooltip("Camera Smooth Factor. Lower = more smooth.")]
     public float SmoothFactor = 0.025f;
@@ -31,7 +34,7 @@ public class EditorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Default drag logic
+        // Default LMB drag logic
         // Orbit around Pivot Point
         if (Input.GetMouseButton(0))
         {
@@ -45,16 +48,36 @@ public class EditorController : MonoBehaviour
             currentVelocity = new Vector2(mouseDelta.x, vertical);
 
             // Rotate the camera around the pivot point
-            transform.RotateAround(pivotPoint, Vector3.up, mouseDelta.x * Sensitivity);
-            transform.RotateAround(pivotPoint, transform.right, vertical * Sensitivity);
+            transform.RotateAround(pivotPoint, Vector3.up, mouseDelta.x * OrbitSensitivity);
+            transform.RotateAround(pivotPoint, transform.right, vertical * OrbitSensitivity);
         }
         else
         {
             // If mouse button is not held, smooth the camera's velocity
             currentVelocity = Vector2.Lerp(currentVelocity, Vector2.zero, SmoothFactor);
 
-            transform.RotateAround(pivotPoint, Vector3.up, currentVelocity.x * Sensitivity);
-            transform.RotateAround(pivotPoint, transform.right, currentVelocity.y * Sensitivity);
+            transform.RotateAround(pivotPoint, Vector3.up, currentVelocity.x * OrbitSensitivity);
+            transform.RotateAround(pivotPoint, transform.right, currentVelocity.y * OrbitSensitivity);
+        }
+
+        // Default RMB drag logic
+        // Move pivot point
+        if (Input.GetMouseButton(1))
+        {
+            // Get mouse movement
+            Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+            // Invert input to reverse drag direction
+            float vertical = -mouseDelta.y;
+            float horizontal = -mouseDelta.x;
+
+            // Move the pivot point
+            pivotPoint += transform.right * horizontal * PivotSensitivity;
+            pivotPoint += transform.up * vertical * PivotSensitivity;
+
+            // Apply the movement to the camera position
+            transform.position += transform.right * horizontal * PivotSensitivity;
+            transform.position += transform.up * vertical * PivotSensitivity;
         }
     }
 }
