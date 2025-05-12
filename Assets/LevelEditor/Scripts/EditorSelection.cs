86796eddcc622cbbf5e7c22ae975fe68f7edd16b
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-05-12 23:47:53 +1000

    Add per-face Voxel selection.

--- Assets/LevelEditor/Scripts/EditorSelection.cs
+++ Assets/LevelEditor/Scripts/EditorSelection.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class EditorSelection : MonoBehaviour
 {
-    // Positions of all selected voxels
-    private List<Vector3Int> selectedVoxels = new();
+    // POSITION        |      NORMAL
+    private Dictionary<Vector3Int, Vector3Int> selectedVoxels = new();
+
     private VoxelWorld voxelWorld;
 
     // Update is called once per frame
@@ -21,22 +22,21 @@ public class EditorSelection : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 // Dont continue if we didnt hit a voxel
-                if (hit.collider.transform.GetComponentInParent<VoxelWorld>() == null)
-                    return;
+                voxelWorld = hit.collider.transform.GetComponentInParent<VoxelWorld>();
+                if (voxelWorld == null) return;
 
-                // Optionally clear selected voxels
+                Vector3Int voxelPos = voxelWorld.WorldToVoxelPosition(hit.collider.transform.position);
+                Voxel voxel = voxelWorld.GetVoxel(voxelPos);
+
+                // Dont continue if we hit an object voxel-type
+                if (voxel.Type != VoxelType.Terrain) return;
+
+                // Clear selection unless shift is held
                 if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.RightShift))
                     selectedVoxels.Clear();
 
-                // Get the Voxel we hit
-                voxelWorld = hit.collider.transform.GetComponentInParent<VoxelWorld>();
-                Voxel voxel = voxelWorld.GetVoxel(voxelWorld.WorldToVoxelPosition(hit.collider.transform.position));
-
-                // Voxel is part of the chamber
-                if (voxel.Type == VoxelType.Terrain)
-                {
-                    selectedVoxels.Add(voxelWorld.WorldToVoxelPosition(hit.collider.transform.position));
-                }
+                Vector3Int normal = Vector3Int.RoundToInt(hit.normal);
+                selectedVoxels[voxelPos] = normal;
             }
         }
 
@@ -44,55 +44,42 @@ public class EditorSelection : MonoBehaviour
         // + Key
         if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && Input.GetKeyDown(KeyCode.Equals))
         {
-            // List to store positions to add and remove after the loop
-            List<Vector3Int> positionsToAdd = new List<Vector3Int>();
-            List<Vector3Int> positionsToRemove = new List<Vector3Int>();
+            Dictionary<Vector3Int, Vector3Int> updatedSelection = new();
 
-            foreach (Vector3Int position in selectedVoxels)
+            foreach (var kvp in selectedVoxels)
             {
-                // Add voxel above
-                Vector3Int newPosition = position + new Vector3Int(0, 1, 0);
-                voxelWorld.AddVoxel(newPosition, voxelWorld.GetVoxel(position));
-                voxelWorld.UpdateVoxelWorld();
+                Vector3Int currentPos = kvp.Key;
+                Vector3Int normal = kvp.Value;
 
-                // Collect the positions to update after the loop
-                positionsToAdd.Add(newPosition);
-                positionsToRemove.Add(position);
+                Vector3Int targetPos = currentPos - normal * -1;
+
+                voxelWorld.AddVoxel(targetPos, voxelWorld.GetVoxel(currentPos));
+                updatedSelection[targetPos] = normal;
             }
 
-            // Update the selectedVoxels list outside the loop
-            foreach (Vector3Int position in positionsToAdd)
-                selectedVoxels.Add(position);
-
-            foreach (Vector3Int position in positionsToRemove)
-                selectedVoxels.Remove(position);
+            voxelWorld.UpdateVoxelWorld();
+            selectedVoxels = updatedSelection;
         }
 
         // - Key
         if (Input.GetKeyDown(KeyCode.Minus))
         {
-            // List to store positions to add and remove after the loop
-            List<Vector3Int> positionsToAdd = new List<Vector3Int>();
-            List<Vector3Int> positionsToRemove = new List<Vector3Int>();
+            Dictionary<Vector3Int, Vector3Int> updatedSelection = new();
 
-
-            foreach (Vector3Int position in selectedVoxels)
+            foreach (var kvp in selectedVoxels)
             {
-                // Remove voxel 
-                voxelWorld.RemoveVoxel(position);
-                voxelWorld.UpdateVoxelWorld();
+                Vector3Int currentPos = kvp.Key;
+                Vector3Int normal = kvp.Value;
 
-                // Collect the positions to update after the loop
-                positionsToAdd.Add(position );
-                positionsToRemove.Add(position);
+                Vector3Int targetPos = currentPos + normal * -1;
+
+                voxelWorld.AddVoxel(targetPos, voxelWorld.GetVoxel(currentPos));
+                voxelWorld.RemoveVoxel(currentPos);
+                updatedSelection[targetPos] = normal;
             }
 
-            // Update the selectedVoxels list outside the loop
-            foreach (Vector3Int position in positionsToAdd)
-                selectedVoxels.Add(position);
-
-            foreach (Vector3Int position in positionsToRemove)
-                selectedVoxels.Remove(position);
+            voxelWorld.UpdateVoxelWorld();
+            selectedVoxels = updatedSelection;
         }
     }
 }
