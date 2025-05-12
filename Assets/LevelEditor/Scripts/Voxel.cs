ad43a52a628173ed2d2e9b25c5f1b1e30218bb9b
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-05-12 21:07:54 +1000

    Setup Voxel Meshes.

--- Assets/LevelEditor/Scripts/Voxel.cs
+++ Assets/LevelEditor/Scripts/Voxel.cs
@@ -8,5 +8,135 @@ public enum VoxelType
 
 public struct Voxel
 {
+    /// <summary>
+    /// The type of this Voxel.
+    /// </summary>
     public VoxelType Type;
+
+    /// <summary>
+    /// The GameObject that represents this Voxel.
+    /// </summary>
+    public GameObject GameObject;
+}
+
+
+/// <summary>
+/// Utility class for voxel data.
+/// </summary>
+public static class VoxelUtility
+{
+    // Backing field
+    private static Mesh _voxelMesh;
+
+    /// <summary>
+    /// The mesh used for all voxels. 
+    /// This can be set to override the default voxel mesh.
+    /// </summary>
+    public static Mesh VoxelMesh { 
+        get 
+        {
+            if (_voxelMesh == null)
+                GenerateVoxelMesh();
+
+            return _voxelMesh;
+        }
+
+        set => _voxelMesh = value; 
+    }
+
+    // Generate the default Voxel Mesh
+    // This gets called by the VoxelMesh getter
+    private static void GenerateVoxelMesh()
+    {
+        // Set default voxel mesh (6 sided cube)
+        _voxelMesh = new Mesh();
+        _voxelMesh.name = "VoxelCube";
+
+        Vector3[] vertices = new Vector3[24]
+        {
+            // Front face
+            new Vector3(-0.5f, -0.5f,  0.5f),
+            new Vector3( 0.5f, -0.5f,  0.5f),
+            new Vector3( 0.5f,  0.5f,  0.5f),
+            new Vector3(-0.5f,  0.5f,  0.5f),
+
+            // Back face
+            new Vector3( 0.5f, -0.5f, -0.5f),
+            new Vector3(-0.5f, -0.5f, -0.5f),
+            new Vector3(-0.5f,  0.5f, -0.5f),
+            new Vector3( 0.5f,  0.5f, -0.5f),
+
+            // Left face
+            new Vector3(-0.5f, -0.5f, -0.5f),
+            new Vector3(-0.5f, -0.5f,  0.5f),
+            new Vector3(-0.5f,  0.5f,  0.5f),
+            new Vector3(-0.5f,  0.5f, -0.5f),
+
+            // Right face
+            new Vector3( 0.5f, -0.5f,  0.5f),
+            new Vector3( 0.5f, -0.5f, -0.5f),
+            new Vector3( 0.5f,  0.5f, -0.5f),
+            new Vector3( 0.5f,  0.5f,  0.5f),
+
+            // Top face
+            new Vector3(-0.5f,  0.5f,  0.5f),
+            new Vector3( 0.5f,  0.5f,  0.5f),
+            new Vector3( 0.5f,  0.5f, -0.5f),
+            new Vector3(-0.5f,  0.5f, -0.5f),
+
+            // Bottom face
+            new Vector3(-0.5f, -0.5f, -0.5f),
+            new Vector3( 0.5f, -0.5f, -0.5f),
+            new Vector3( 0.5f, -0.5f,  0.5f),
+            new Vector3(-0.5f, -0.5f,  0.5f),
+        };
+
+        int[] indices = new int[36]
+        {
+            0, 2, 1, 0, 3, 2,         // Front
+            4, 6, 5, 4, 7, 6,         // Back
+            8,10, 9, 8,11,10,         // Left
+            12,14,13,12,15,14,        // Right
+            16,18,17,16,19,18,        // Top
+            20,22,21,20,23,22         // Bottom
+        };
+
+        Vector3[] normals = new Vector3[24]
+        {       
+            Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward,     // Front
+            Vector3.back, Vector3.back, Vector3.back, Vector3.back,                 // Back
+            Vector3.left, Vector3.left, Vector3.left, Vector3.left,                 // Left
+            Vector3.right, Vector3.right, Vector3.right, Vector3.right,             // Right
+            Vector3.up, Vector3.up, Vector3.up, Vector3.up,                         // Top
+            Vector3.down, Vector3.down, Vector3.down, Vector3.down                  // Bottom
+        };
+
+        Vector2[] uvs = new Vector2[24]
+        {
+            new Vector2(0, 0), new Vector2(1, 0),
+            new Vector2(1, 1), new Vector2(0, 1),
+
+            new Vector2(0, 0), new Vector2(1, 0),
+            new Vector2(1, 1), new Vector2(0, 1),
+
+            new Vector2(0, 0), new Vector2(1, 0),
+            new Vector2(1, 1), new Vector2(0, 1),
+
+            new Vector2(0, 0), new Vector2(1, 0),
+            new Vector2(1, 1), new Vector2(0, 1),
+
+            new Vector2(0, 0), new Vector2(1, 0),
+            new Vector2(1, 1), new Vector2(0, 1),
+
+            new Vector2(0, 0), new Vector2(1, 0),
+            new Vector2(1, 1), new Vector2(0, 1)
+        };
+
+        _voxelMesh.vertices = vertices;
+        _voxelMesh.triangles = indices;
+        _voxelMesh.normals = normals;
+        _voxelMesh.uv = uvs;
+
+        _voxelMesh.RecalculateBounds();
+    }
 }
--- Assets/LevelEditor/Scripts/VoxelWorld.cs
+++ Assets/LevelEditor/Scripts/VoxelWorld.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -9,9 +10,36 @@ public class VoxelWorld : MonoBehaviour
     [Tooltip("The size of each voxel.")]
     public float VoxelSize = 1.0f;
 
+    // Voxel World
+    private Dictionary<Vector3Int, Voxel> voxelWorldData = new Dictionary<Vector3Int, Voxel>();
+
+    /// <summary>
+    /// Get a voxel from it's position.
+    /// </summary>
+    public Voxel GetVoxel(Vector3Int position) => voxelWorldData[position];
+
+    /// <summary>
+    /// Add a voxel to the world.
+    /// </summary>
+    public void AddVoxel(Vector3Int position, Voxel voxel) => voxelWorldData[position] = voxel;
+
+    /// <summary>
+    /// Remove a voxel from the world.
+    /// </summary>
+    public void RemoveVoxel(Vector3Int position) => voxelWorldData.Remove(position);
+
+    // Update the voxel world (render it, etc)
+    public void UpdateVoxelWorld()
+    {
+        foreach (var voxel in voxelWorldData.Values)
+        {
+            voxel.UpdateVoxel();
+        }
+    }
+
     private void OnEnable()
     {
-        
+        UpdateVoxelWorld();
     }
 
     private void OnDisable()
